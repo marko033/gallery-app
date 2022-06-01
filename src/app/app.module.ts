93273a4b4ba591696bec9c0988cd929e2a1c0c4e
Marek <marko033@gmail.com>
2022-06-01 19:12:37 +0200

    pridanie lightboxu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { LightboxModule } from 'ngx-lightbox';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
@@ -22,7 +24,8 @@ import { AddCardComponent } from './add-card/add-card.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    LightboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Lightbox } from 'ngx-lightbox';
+
 import { ApiService } from '../api.service';
 
 @Component({
@@ -16,23 +18,68 @@ export class GalleryComponent implements OnInit {
   file: any;
   files: any;
 
+  private _album: Array<any> = [];
+
   constructor(
     private route: ActivatedRoute,
-    private apiService: ApiService
+    private apiService: ApiService,
+    private _lightbox: Lightbox
     ) {
     this.route.params.subscribe(params => {
       this.galleryName = params['name'] === undefined ? 'movies' : params['name'];
     });
+
    }
 
   ngOnInit(): void {
     this.getImages();
   }
 
+  test(images: any) {
+    console.log(images);
+
+    for(const image of images) {
+      const src = 'http://api.programator.sk/images/1212x909/' + image.fullpath;
+      const caption = image.name;
+      const thumb = 'http://api.programator.sk/images/304x295/' + image.fullpath;
+      const album = {
+         src: src,
+         caption: caption,
+         thumb: thumb
+      };
+
+      this._album.push(album);
+    }
+
+    for (let i = 1; i <= 4; i++) {
+      const src = 'demo/img/image' + i + '.jpg';
+      const caption = 'Image ' + i + ' caption here';
+      const thumb = 'demo/img/image' + i + '-thumb.jpg';
+      const album = {
+         src: src,
+         caption: caption,
+         thumb: thumb
+      };
+
+      this._album.push(album);
+    }
+  }
+
+  open(index: number): void {
+    // open lightbox
+    this._lightbox.open(this._album, index);
+  }
+
+  close(): void {
+    // close lightbox programmatically
+    this._lightbox.close();
+  }
+
   getImages() {
     this.apiService.getGalleryPath(this.galleryName)
       .subscribe( data => {
         this.data = data;
+        this.test(this.data.images);
       },
       error => {
         console.error(error);
